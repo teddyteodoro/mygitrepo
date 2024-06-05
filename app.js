3e00134fe98f57667de884741fe5f7a909010045
teddyteodoro <teddycaguioa@gmail.com>
2024-06-05 15:10:22 +0800

    Add files via upload

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var quotesRouter = require('./routes/quotes');
+var searchRouter = require('./routes/search');
 
 var app = express();
 
@@ -16,5 +17,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/quotes', quotesRouter);
+app.use('/search', searchRouter);
 
 module.exports = app;
