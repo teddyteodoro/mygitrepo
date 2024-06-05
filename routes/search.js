358de62ed410062e383c14d8666c2dd3aa08f9ce
teddyteodoro <teddycaguioa@gmail.com>
2024-06-05 17:33:37 +0800

    Add files via upload

--- routes/search.js
+++ routes/search.js
@@ -1,12 +1,11 @@
 const express = require("express");
 const router = express.Router();
-const axios = require('axios');
-
+const axios = require("axios");
 
 /* GET quotes listing. */
 router.get("/", async function (req, res, next) {
   try {
-    res.json({hi: 'hello'});
+    res.json({ hi: "hello" });
   } catch (err) {
     console.error(`Error while getting search items `, err.message);
     res.status(err.statusCode || 500).json({ message: err.message });
@@ -14,21 +13,19 @@ router.get("/", async function (req, res, next) {
 });
 
 function getStore() {
-
-  return {results: []};
   return [
     {
       id: "suiteanswers_doc_1",
       title: "Installing the Workforce Management SuiteApp",
       text: "As an Administrator, install the Workforce Management SuiteApp using the following details: Bundle Name: Adi Insights",
-      url: "https://suiteanswers.custhelp.com/app/answers/detail/a_id/108569/loc/en_US"
+      url: "https://suiteanswers.custhelp.com/app/answers/detail/a_id/108569/loc/en_US",
     },
     {
       id: "suiteanswers_doc_2",
       title:
         "Rolling back the SuitePeople Time Clock Windows Application to AdiClock",
       text: "helo",
-      url: "https://suiteanswers.custhelp.com/app/answers/detail/a_id/1016217/loc/en_US"
+      url: "https://suiteanswers.custhelp.com/app/answers/detail/a_id/1016217/loc/en_US",
     },
   ];
 }
