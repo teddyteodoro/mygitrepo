397d55b37abda3608b106568a067e29dead4df3e
teddyteodoro <teddycaguioa@gmail.com>
2024-06-05 21:29:50 +0800

    Add files via upload

---
+++ app.js
@@ -0,0 +1,26 @@
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+var cors = require("cors");
+
+var indexRouter = require('./routes/index');
+var quotesRouter = require('./routes/quotes');
+var searchRouter = require('./routes/search');
+
+var app = express();
+
+app.use(cors());
+
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/quotes', quotesRouter);
+app.use('/search', searchRouter);
+
+module.exports = app;
---
+++ routes/search.js
@@ -0,0 +1,74 @@
+const express = require("express");
+const router = express.Router();
+// const axios = require("axios");
+
+/* POST search */
+router.post("/", async function (req, res, next) {
+  try {
+    const userQuery = req.body.query;
+    const results = search(userQuery);
+    results =[]
+    res.json(results);
+
+    // res.json(await quotes.create(req.body));
+  } catch (err) {
+    console.error(`Error while posting search items `, err.message);
+    res.status(err.statusCode || 500).json({ message: err.message });
+  }
+});
+
+function search(searchPhrase) {
+
+  if ((searchPhrase || "") == "") {
+    return [];
+  }
+  const hasSpace = searchPhrase.indexOf(" ") > -1;
+  const searchWords = hasSpace ? searchPhrase.split(" ") : [searchPhrase];
+  const allSearchEntries = getStore();
+  return allSearchEntries.filter((entry) => {
+    for (let i = 0; i < searchWords.length; i++) {
+      let searchWord = searchWords[i];
+      searchWord = searchWord.toLowerCase();
+      if (entry.title.toLowerCase().indexOf(searchWord) > -1) {
+        return true;
+      }
+      if (entry.text.toLowerCase().indexOf(searchWord) > -1) {
+        return true;
+      }
+    }
+    return false;
+  });
+}
+
+function getStore() {
+  return [
+    {
+      "id": "suiteanswers_doc_1",
+      "title": "Installing the Workforce Management SuiteApp",
+      "text": "As an Administrator, install the Workforce Management SuiteApp using the following details: Bundle Name: Adi Insights",
+      "url": "https://suiteanswers.custhelp.com/app/answers/detail/a_id/108569/loc/en_US",
+    },
+    {
+      id: "suiteanswers_doc_2",
+      title:
+        "Rolling back the SuitePeople Time Clock Windows Application to AdiClock",
+      text: "helo",
+      url: "https://suiteanswers.custhelp.com/app/answers/detail/a_id/1016217/loc/en_US",
+    },
+  ];
+}
+
+
+
+
+/* GET quotes listing. */
+router.get("/", async function (req, res, next) {
+  try {
+    res.json({ hi: "hello" });
+  } catch (err) {
+    console.error(`Error while getting search items `, err.message);
+    res.status(err.statusCode || 500).json({ message: err.message });
+  }
+});
+
+module.exports = router;
