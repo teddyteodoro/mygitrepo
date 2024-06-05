1a7de4669e6739dae731086b0e48d8dee5bd8584
teddyteodoro <teddycaguioa@gmail.com>
2024-06-05 17:48:48 +0800

    Update search.js

--- routes/search.js
+++ routes/search.js
@@ -2,34 +2,23 @@ const express = require("express");
 const router = express.Router();
 // const axios = require("axios");
 
-/* GET quotes listing. */
-router.get("/", async function (req, res, next) {
+/* POST search */
+router.post("/", async function (req, res, next) {
   try {
-    res.json({ hi: "hello" });
+    const userQuery = req.body.query;
+    const results = search(userQuery);
+    results =[]
+    res.json(results);
+
+    // res.json(await quotes.create(req.body));
   } catch (err) {
-    console.error(`Error while getting search items `, err.message);
+    console.error(`Error while posting search items `, err.message);
     res.status(err.statusCode || 500).json({ message: err.message });
   }
 });
 
-function getStore() {
-  return [
-    {
-      id: "suiteanswers_doc_1",
-      title: "Installing the Workforce Management SuiteApp",
-      text: "As an Administrator, install the Workforce Management SuiteApp using the following details: Bundle Name: Adi Insights",
-      url: "https://suiteanswers.custhelp.com/app/answers/detail/a_id/108569/loc/en_US",
-    },
-    {
-      id: "suiteanswers_doc_2",
-      title:
-        "Rolling back the SuitePeople Time Clock Windows Application to AdiClock",
-      text: "helo",
-      url: "https://suiteanswers.custhelp.com/app/answers/detail/a_id/1016217/loc/en_US",
-    },
-  ];
-}
 function search(searchPhrase) {
+
   if ((searchPhrase || "") == "") {
     return [];
   }
@@ -51,16 +40,33 @@ function search(searchPhrase) {
   });
 }
 
-/* POST search */
-router.post("/", async function (req, res, next) {
-  try {
-    const userQuery = req.body.query;
-    const results = search(userQuery);
-    res.json({results: []});
+function getStore() {
+  return [
+    {
+      "id": "suiteanswers_doc_1",
+      "title": "Installing the Workforce Management SuiteApp",
+      "text": "As an Administrator, install the Workforce Management SuiteApp using the following details: Bundle Name: Adi Insights",
+      "url": "https://suiteanswers.custhelp.com/app/answers/detail/a_id/108569/loc/en_US",
+    },
+    {
+      id: "suiteanswers_doc_2",
+      title:
+        "Rolling back the SuitePeople Time Clock Windows Application to AdiClock",
+      text: "helo",
+      url: "https://suiteanswers.custhelp.com/app/answers/detail/a_id/1016217/loc/en_US",
+    },
+  ];
+}
 
-    // res.json(await quotes.create(req.body));
+
+
+
+/* GET quotes listing. */
+router.get("/", async function (req, res, next) {
+  try {
+    res.json({ hi: "hello" });
   } catch (err) {
-    console.error(`Error while posting search items `, err.message);
+    console.error(`Error while getting search items `, err.message);
     res.status(err.statusCode || 500).json({ message: err.message });
   }
 });
