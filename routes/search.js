7444c2e0e9efe28c0e56bad38befae4bc80da8e6
teddyteodoro <teddycaguioa@gmail.com>
2024-06-05 16:52:57 +0800

    Add files via upload

--- routes/search.js
+++ routes/search.js
@@ -18,16 +18,14 @@ function getStore() {
       id: "suiteanswers_doc_1",
       title: "Installing the Workforce Management SuiteApp",
       text: "As an Administrator, install the Workforce Management SuiteApp using the following details: Bundle Name: Adi Insights \nBundle ID: 317115 \nFor more information about how to install a SuiteApp in your account, see Installing a Bundle. \nTo continue setting up WFM, see Assigning the Adi Insights Integration Role.",
-      url: "https://suiteanswers.custhelp.com/app/answers/detail/a_id/108569/loc/en_US",
-      created_at: "2023-11-13T20:09:31Z",
+      url: "https://suiteanswers.custhelp.com/app/answers/detail/a_id/108569/loc/en_US"
     },
     {
       id: "suiteanswers_doc_2",
       title:
         "Rolling back the SuitePeople Time Clock Windows Application to AdiClock",
       text: "SuitePeople Workforce management customers who upgrade to SuitePeople Time Clock version 2.0.0.0 for Windows have an option to roll back to the previous AdiClock version. \nIn line with the acquisition of Adi Insights by Oracle, the AdiClock Windows app is targeted for an upgrade that rebrands its visual components to align with the Oracle NetSuite colors and themes and rename the application from AdiClock to SuitePeople Time Clock. \nStarting June 5, 2024, Oracle will upgrade the AdiClock Windows application to SuitePeople Time Clock version 2.0.0.0.",
-      url: "https://suiteanswers.custhelp.com/app/answers/detail/a_id/1016217/loc/en_US",
-      created_at: "2023-11-25T20:09:31Z",
+      url: "https://suiteanswers.custhelp.com/app/answers/detail/a_id/1016217/loc/en_US"
     },
   ];
 }
