2565a903d40cb80f22b12fe0c550a915e164b769
teddyteodoro <teddycaguioa@gmail.com>
2024-06-05 15:33:23 +0800

    Add files via upload

--- app.js
+++ app.js
@@ -2,12 +2,16 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var cors = require("cors");
 
 var indexRouter = require('./routes/index');
 var quotesRouter = require('./routes/quotes');
 var searchRouter = require('./routes/search');
 
 var app = express();
+
+app.use(cors());
+
 
 app.use(logger('dev'));
 app.use(express.json());
