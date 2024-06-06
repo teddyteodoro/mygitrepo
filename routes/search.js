206501ad5d352165bee51d61921fed8da8cafa38
teddyteodoro <teddycaguioa@gmail.com>
2024-06-06 10:33:10 +0800

    Update search.js

--- routes/search.js
+++ routes/search.js
@@ -4,7 +4,26 @@ const router = express.Router();
 
 /* POST search */
 router.post("/", async function (req, res, next) {
- res.json([]);
+
+ var results = {  
+  "results": [  
+  {  
+    "id": "expense_doc_1",  
+    "title": "Expense Policies for external travel",  
+    "text": "You can expense any work-related...",  
+    "url": "https://internal-site.com/expensing"  
+    "created_at": "2023-11-25T20:09:31Z",  
+  },  
+   {  
+    "id": "expense_doc_2",  
+    "title": "Expense Policies for internal travel",  
+    "text": "You can expense any work-related...",  
+    "created_at": "2023-11-25T20:09:31Z",  
+  }
+  ]  
+}
+ 
+ res.json(results);
   return ;
   try {
     const userQuery = req.body.query;
