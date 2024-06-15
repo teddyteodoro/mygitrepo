d6e31ff9d6c43d3a5be60ec254b61bfb16d8f39a
teddyteodoro <teddycaguioa@gmail.com>
2024-06-15 22:00:55 +0800

    Update search.js

--- routes/search.js
+++ routes/search.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-// const axios = require("axios");
+const { V4 } = require('paseto');
 
 /* POST search */
 router.post("/", async function (req, res, next) {
